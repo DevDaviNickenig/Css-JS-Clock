80b4d7f440c355e596598bef39770d0288473551
Davi Nickenig Cassitas Barbosa <devdavinickenig@gmail.com>
2023-10-14 00:11:48 -0300

    Segundo dia do 30 days of JS
    Commit inicial, já com html e Css.
    
    JavaScript com os ponteiros e hora atual.

---
+++ script.js
@@ -0,0 +1,10 @@
+const ponteiroHora = document.querySelector('.ponteiro__hora');
+const ponteiroMin = document.querySelector('.ponteiro__min');
+const ponteiroSeg = document.querySelector('.ponteiro__seg');
+
+console.log(ponteiroHora, ponteiroMin, ponteiroSeg);
+
+
+const horaAtual = new Date();
+
+console.log(horaAtual)
